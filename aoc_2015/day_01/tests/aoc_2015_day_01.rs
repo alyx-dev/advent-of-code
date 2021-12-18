86964c318b207a437f007efb048693fb6aa86538
Alyx <alyx-dev@pm.me>
2021-12-18 21:22:13 +0100

    AoC 2015 day 01

--- aoc_2015/day_01/tests/aoc_2015_day_01.rs
+++ aoc_2015/day_01/tests/aoc_2015_day_01.rs
@@ -5,13 +5,13 @@ use aoc_2015_day_01::*;
 #[test]
 fn part_one_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_one(input)?, 1);
+    assert_eq!(part_one(input)?, 138);
     Ok(())
 }
 
 #[test]
 fn part_two_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_two(input)?, 2);
+    assert_eq!(part_two(input)?, 1771);
     Ok(())
 }
