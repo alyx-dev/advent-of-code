5a604acaf7365b81ce1da7f4a8f81f14f2d604ae
Alyx <alyx-dev@pm.me>
2021-12-20 21:42:57 +0100

    AoC 2015 day 04

--- aoc_2015/day_04/src/lib.rs
+++ aoc_2015/day_04/src/lib.rs
@@ -1,12 +1,47 @@
 use anyhow::Result;
+use md5::{digest::generic_array::GenericArray, Digest, Md5};
 
 pub const YEAR: u32 = 2015;
-pub const DAY: u32 = 4;
+pub const DAY: u32 = 3;
 
-pub fn part_one(_: &str) -> Result<u32> {
-    Ok(1)
+pub fn part_one(input: &str) -> Result<u32> {
+    let input = input.trim();
+    let mut hasher = Md5::new();
+    let mut array = GenericArray::default();
+    for int in 1.. {
+        hasher.update(input);
+        hasher.update(int.to_string());
+        hasher.finalize_into_reset(&mut array);
+        if array[..2] == [0, 0] && array[2] <= 0x0F {
+            return Ok(int);
+        }
+    }
+    unreachable!()
 }
 
-pub fn part_two(_: &str) -> Result<u32> {
-    Ok(2)
+pub fn part_two(input: &str) -> Result<u32> {
+    let input = input.trim();
+    let mut hasher = Md5::new();
+    let mut array = GenericArray::default();
+    for int in 1.. {
+        hasher.update(input);
+        hasher.update(int.to_string());
+        hasher.finalize_into_reset(&mut array);
+        if array[..3] == [0, 0, 0] {
+            return Ok(int);
+        }
+    }
+    unreachable!()
+}
+
+#[test]
+fn part_one_example1() -> Result<()> {
+    assert_eq!(part_one("abcdef")?, 609043);
+    Ok(())
+}
+
+#[test]
+fn part_one_example2() -> Result<()> {
+    assert_eq!(part_one("pqrstuv")?, 1048970);
+    Ok(())
 }
